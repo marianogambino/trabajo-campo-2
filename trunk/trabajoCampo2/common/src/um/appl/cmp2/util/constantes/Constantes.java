f71eb629b966a37256081e39897c21e6363d387d
marianogambino@gmail.com <marianogambino@gmail.com@8f3cd698-a9d3-fb52-5ddb-903d10f7ac17>
2011-04-30 17:15:29 +0000

    Commit del Common

---
+++ trunk/trabajoCampo2/common/src/um/appl/cmp2/util/constantes/Constantes.java
@@ -0,0 +1,16 @@
+package um.appl.cmp2.util.constantes;
+
+public class Constantes 
+{
+	//
+	public static String CONECTION_HIBERNATE = "hibernate_jdbc";
+	
+	//
+	public static String BARRA_INVERTIDA = "/";
+	
+	public static String ERROR = "error";
+	public static String OK = "OK";
+	public static String ERROR_USUARIO = "Nombre de Usuario o Constraseña Invalida";
+	public static String MAIN = "main";
+	
+}
