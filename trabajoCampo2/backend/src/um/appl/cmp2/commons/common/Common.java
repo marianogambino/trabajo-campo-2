15abd7ac8b073eb19ae4d8e8cbe79bc96aaa1975
marianogambino@gmail.com <marianogambino@gmail.com@8f3cd698-a9d3-fb52-5ddb-903d10f7ac17>
2011-05-01 00:16:59 +0000

    commit del Backend

---
+++ trabajoCampo2/backend/src/um/appl/cmp2/commons/common/Common.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package um.appl.cmp2.commons.common;
+
+import java.io.Serializable;
+
+/**
+ * @author Mariano
+ *
+ */
+@SuppressWarnings("serial")
+public class Common implements Serializable {
+
+}
